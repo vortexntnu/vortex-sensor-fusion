c1c2e40f761e9f31e7846564919dda9fc5c55ac2
Christopher Strøm <chstr@stud.ntnu.no>
2023-02-20 19:34:55 +0100

    Rewrite GMM detector
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- detectors/pcl_detector/include/pcl_detector/detectors/gmm_detector.hpp
+++ detectors/pcl_detector/include/pcl_detector/detectors/gmm_detector.hpp
@@ -1,47 +1,34 @@
-/**
- * @file gmm_detector.hpp
- * @brief This file defines a GMM detector that inherits from the IPclDetector interface.
- */
-
 #ifndef GMM_DETECTOR_H
 #define GMM_DETECTOR_H
 
-#include <pcl/segmentation/gmm.h>
+#include <vector>
 
-#include "pcl_detector.hpp"
+#include <pcl/common/distances.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/segmentation/growing_region.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include "pcl_detector/pcl_detector.h"
 
 namespace pcl_detector {
 
-/**
- * @class GMMDetector
- * @brief A GMM detector that implements the IPclDetector interface.
- */
 class GMMDetector : public IPclDetector {
-
 public:
-    /**
-     * @brief Constructs a GMM detector with the given parameters.
-     * @param num_gaussians The number of Gaussian components to fit to the data.
-     * @param max_iterations The maximum number of iterations to run the GMM algorithm.
-     * @param convergence_threshold The convergence threshold for the GMM algorithm.
-     */
-    GMMDetector(int num_gaussians, int max_iterations, double convergence_threshold)
-        : num_gaussians_(num_gaussians), max_iterations_(max_iterations), convergence_threshold_(convergence_threshold) {}
+    GMMDetector(int num_clusters, int max_iterations);
 
-    /**
-     * @brief Detects clusters in the given point cloud using GMM.
-     * @param points The input point cloud.
-     * @return The detected clusters as a point cloud.
-     */
     pcl::PointCloud<pcl::PointXYZ> get_detections(const pcl::PointCloud<pcl::PointXYZ>& points) override;
 
 private:
-    int num_gaussians_; /**< The number of Gaussian components to fit to the data. */
-    int max_iterations_; /**< The maximum number of iterations to run the GMM algorithm. */
-    double convergence_threshold_; /**< The convergence threshold for the GMM algorithm. */
-
+    int num_clusters_;
+    int max_iterations_;
 };
 
-}; // namespace pcl_detector
+} // namespace pcl_detector
 
 #endif // GMM_DETECTOR_H
