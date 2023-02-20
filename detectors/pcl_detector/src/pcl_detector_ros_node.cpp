a97e9ec3df1553aea20f605af8dd1d19da0379b7
Christopher Strøm <chstr@stud.ntnu.no>
2023-02-20 17:55:56 +0100

    Use new detector
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- detectors/pcl_detector/src/pcl_detector_ros_node.cpp
+++ detectors/pcl_detector/src/pcl_detector_ros_node.cpp
@@ -1,4 +1,4 @@
-#include "pcl_detector/detectors/euclidean_clustering.hpp"
+#include "pcl_detector/detectors/dbscan_detector.hpp"
 #include "pcl_detector/pcl_detector_ros.hpp"
 
 using namespace pcl_detector;
@@ -8,14 +8,11 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "pcl_detector");
     ros::NodeHandle nh;
 
-    double tolerance = 1.0; // Meters
-    int min_size = 10;
-    int max_size = 10000;
-    double range = 100; // Meters
+    double eps = 1.0; // Meters
+    int min_points = 10;
     float leaf_size = 0.05; // Meters
 
-    std::shared_ptr<IPclDetector> detector = std::make_shared<EuclideanClusteringDetector>(tolerance, min_size,
-        max_size, range);
+    std::shared_ptr<IPclDetector> detector = std::make_shared<DBSCANDetector>(eps, min_points);
 
     PclDetectorRos ros_detector{ nh, detector, leaf_size };
 
