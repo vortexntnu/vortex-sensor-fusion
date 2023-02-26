e94f6bfcf1c45bbc3ed33bded5f87346e0b65d08
Christopher Strøm <chstr@stud.ntnu.no>
2023-02-26 15:05:37 +0100

    Hotfix: Changes that were overwritten at the Stormtrooper incident
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- detectors/pcl_detector/src/dbscan_detector.cpp
+++ detectors/pcl_detector/src/dbscan_detector.cpp
@@ -2,37 +2,70 @@
 
 namespace pcl_detector {
 
+
 pcl::PointCloud<pcl::PointXYZ> DBSCANDetector::get_detections(const pcl::PointCloud<pcl::PointXYZ>& points) {
     pcl::PointCloud<pcl::PointXYZ> detections;
 
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+    // create a FLANN k-d tree for efficient approximate nearest neighbor search
+    pcl::search::FlannSearch<pcl::PointXYZ>::Ptr tree(new pcl::search::FlannSearch<pcl::PointXYZ>);
     tree->setInputCloud(points.makeShared());
 
-    std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-    ec.setClusterTolerance(eps_);
-    ec.setMinClusterSize(min_points_);
-    ec.setMaxClusterSize(std::numeric_limits<int>::max());
-    ec.setSearchMethod(tree);
-    ec.setInputCloud(points.makeShared());
-    ec.extract(cluster_indices);
+    // initialize labels
+    std::vector<int> labels(points.size(), -1); // -1 for noise points, >= 0 for clustered points
 
-    for (const auto& indices : cluster_indices) {
-        pcl::PointXYZ centroid;
-        centroid.getArray3fMap() = Eigen::Array3f::Zero();
-        for (const auto& index : indices.indices) {
-            const auto& point = points[index];
-            centroid.x += point.x;
-            centroid.y += point.y;
-            centroid.z += point.z;
+    // initialize cluster ID and noise label
+    int cluster_id = 0;
+    const int noise_label = -1;
+
+    // iterate through all points
+    for (int i = 0; i < points.size(); i++) {
+        if (labels[i] != -1) { // skip points that have already been assigned to a cluster
+            continue;
         }
-        centroid.x /= indices.indices.size();
-        centroid.y /= indices.indices.size();
-        centroid.z /= indices.indices.size();
-        detections.push_back(centroid);
+
+        std::vector<int> neighbors;
+        std::vector<float> distances;
+
+        // find all neighbors of this point within eps distance using FLANN
+        tree->radiusSearch(points[i], eps_, neighbors, distances);
+
+        if (neighbors.size() < min_points_) { // assign as noise if the point does not have enough neighbors
+            labels[i] = noise_label;
+            continue;
+        }
+
+        // assign a new cluster ID and add this point to it
+        labels[i] = cluster_id;
+        detections.push_back(points[i]);
+
+        // expand the cluster by adding all reachable neighbors
+        expand_cluster(points[i], cluster_id, neighbors, labels, detections, tree, eps_, min_points_);
+
+        cluster_id++; // move to next cluster
     }
 
     return detections;
+}
+
+void DBSCANDetector::expand_cluster(const pcl::PointXYZ& point, int cluster_id, const std::vector<int>& neighbors,
+                                    std::vector<int>& labels, pcl::PointCloud<pcl::PointXYZ>& detections,
+                                    const pcl::search::Search<pcl::PointXYZ>::Ptr& tree, float eps, int min_points) {
+    // expand the cluster by adding all reachable neighbors
+    for (int j = 0; j < neighbors.size(); j++) {
+        if (labels[neighbors[j]] == noise_label) { // assign to cluster if neighbor was previously noise
+            labels[neighbors[j]] = cluster_id;
+            detections.push_back(points[neighbors[j]]);
+        } else if (labels[neighbors[j]] == -1) { // mark as visited and add to neighbor list
+            labels[neighbors[j]] = cluster_id;
+            detections.push_back(points[neighbors[j]]);
+            std::vector<int> more_neighbors;
+            std::vector<float> more_distances;
+            tree->radiusSearch(points[neighbors[j]], eps, more_neighbors, more_distances);
+            if (more_neighbors.size() >= min_points) { // add new neighbors to neighbor list if they meet min_points requirement
+                neighbors.insert(neighbors.end(), more_neighbors.begin(), more_neighbors.end());
+            }
+        }
+    }
 }
 
 }; // namespace pcl_detector
