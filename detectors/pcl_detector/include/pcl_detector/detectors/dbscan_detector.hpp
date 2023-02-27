d1cb3203bee619e9050e19759e12c50056f41a01
Christopher Strøm <chstr@stud.ntnu.no>
2023-02-27 18:39:50 +0100

    Add updated db
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- detectors/pcl_detector/include/pcl_detector/detectors/dbscan_detector.hpp
+++ detectors/pcl_detector/include/pcl_detector/detectors/dbscan_detector.hpp
@@ -6,41 +6,23 @@
 #ifndef DBSCAN_DETECTOR_H
 #define DBSCAN_DETECTOR_H
 
-#include <pcl/segmentation/extract_clusters.h>
-
 #include "pcl_detector/pcl_detector.hpp"
+
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/filters/extract_indices.h>
 
 namespace pcl_detector {
 
-/**
- * @class DBSCANDetector
- * @brief A DBSCAN detector that implements the IPclDetector interface.
- */
 class DBSCANDetector : public IPclDetector {
 
 public:
-    /**
-     * @brief Constructs a DBSCAN detector with the given parameters.
-     * @param eps The maximum distance between two points for them to be considered as part of the same cluster.
-     * @param min_points The minimum number of points for a cluster to be considered valid.
-     */
-    DBSCANDetector(double eps, int min_points) : eps_(eps), min_points_(min_points) {}
+    DBSCANDetector(float eps, int min_points) : m_eps(eps), m_min_points(min_points) {}
 
-    /**
-     * @brief Detects clusters in the given point cloud using DBSCAN.
-     * @param points The input point cloud.
-     * @return The detected clusters as a point cloud.
-     */
     pcl::PointCloud<pcl::PointXYZ> get_detections(const pcl::PointCloud<pcl::PointXYZ>& points) override;
 
 private:
-    double eps_; /**< The maximum distance between two points for them to be considered as part of the same cluster. */
-    int min_points_; /**< The minimum number of points for a cluster to be considered valid. */
-
-    void expand_cluster(const pcl::PointXYZ& point, int cluster_id, const std::vector<int>& neighbors,
-                        std::vector<int>& labels, pcl::PointCloud<pcl::PointXYZ>& detections,
-                        const pcl::search::Search<pcl::PointXYZ>::Ptr& tree, float eps, int min_points);
-
+    float m_eps;
+    int m_min_points;
 };
 
 }; // namespace pcl_detector
--- detectors/pcl_detector/src/dbscan_detector.cpp
+++ detectors/pcl_detector/src/dbscan_detector.cpp
@@ -2,70 +2,77 @@
 
 namespace pcl_detector {
 
-
-pcl::PointCloud<pcl::PointXYZ> DBSCANDetector::get_detections(const pcl::PointCloud<pcl::PointXYZ>& points) {
-    pcl::PointCloud<pcl::PointXYZ> detections;
-
-    // create a FLANN k-d tree for efficient approximate nearest neighbor search
-    pcl::search::FlannSearch<pcl::PointXYZ>::Ptr tree(new pcl::search::FlannSearch<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ> DBSCAN::get_detections(const pcl::PointCloud<pcl::PointXYZ>& points) {
+    // Create a KD-Tree
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     tree->setInputCloud(points.makeShared());
 
-    // initialize labels
-    std::vector<int> labels(points.size(), -1); // -1 for noise points, >= 0 for clustered points
+    // Initialize point labels to be unclassified
+    std::vector<int> labels(points.size(), -1);
+    int label = 0;
 
-    // initialize cluster ID and noise label
-    int cluster_id = 0;
-    const int noise_label = -1;
-
-    // iterate through all points
-    for (int i = 0; i < points.size(); i++) {
-        if (labels[i] != -1) { // skip points that have already been assigned to a cluster
-            continue;
+    // Iterate through each point in the point cloud
+    for (int i = 0; i < points.size(); ++i) {
+        if (labels[i] != -1) {
+            continue; // Already classified, skip to next point
         }
 
         std::vector<int> neighbors;
-        std::vector<float> distances;
+        tree->radiusSearch(i, m_eps, neighbors); // Find neighbors within radius eps
 
-        // find all neighbors of this point within eps distance using FLANN
-        tree->radiusSearch(points[i], eps_, neighbors, distances);
-
-        if (neighbors.size() < min_points_) { // assign as noise if the point does not have enough neighbors
-            labels[i] = noise_label;
+        if (neighbors.size() < m_min_points) {
+            labels[i] = 0; // Mark as noise
             continue;
         }
 
-        // assign a new cluster ID and add this point to it
-        labels[i] = cluster_id;
-        detections.push_back(points[i]);
+        label++; // Start a new cluster
+        labels[i] = label;
 
-        // expand the cluster by adding all reachable neighbors
-        expand_cluster(points[i], cluster_id, neighbors, labels, detections, tree, eps_, min_points_);
-
-        cluster_id++; // move to next cluster
-    }
-
-    return detections;
-}
-
-void DBSCANDetector::expand_cluster(const pcl::PointXYZ& point, int cluster_id, const std::vector<int>& neighbors,
-                                    std::vector<int>& labels, pcl::PointCloud<pcl::PointXYZ>& detections,
-                                    const pcl::search::Search<pcl::PointXYZ>::Ptr& tree, float eps, int min_points) {
-    // expand the cluster by adding all reachable neighbors
-    for (int j = 0; j < neighbors.size(); j++) {
-        if (labels[neighbors[j]] == noise_label) { // assign to cluster if neighbor was previously noise
-            labels[neighbors[j]] = cluster_id;
-            detections.push_back(point[neighbors[j]]);
-        } else if (labels[neighbors[j]] == -1) { // mark as visited and add to neighbor list
-            labels[neighbors[j]] = cluster_id;
-            detections.push_back(point[neighbors[j]]);
-            std::vector<int> more_neighbors;
-            std::vector<float> more_distances;
-            tree->radiusSearch(point[neighbors[j]], eps, more_neighbors, more_distances);
-            if (more_neighbors.size() >= min_points) { // add new neighbors to neighbor list if they meet min_points requirement
-                neighbors.insert(neighbors.end(), more_neighbors.begin(), more_neighbors.end());
+        // Iterate through neighbors and expand cluster
+        for (int j = 0; j < neighbors.size(); ++j) {
+            int neighbor = neighbors[j];
+            if (labels[neighbor] == 0) {
+                labels[neighbor] = label; // Convert noise to border point
+            } else if (labels[neighbor] != -1) {
+                continue; // Already classified, skip to next neighbor
+            }
+            labels[neighbor] = label; // Add neighbor to current cluster
+            std::vector<int> sub_neighbors;
+            tree->radiusSearch(neighbor, m_eps, sub_neighbors);
+            if (sub_neighbors.size() >= m_min_points) {
+                neighbors.insert(neighbors.end(), sub_neighbors.begin(), sub_neighbors.end());
             }
         }
     }
+
+    // Extract the clusters from the point cloud
+    std::vector<pcl::PointCloud<pcl::PointXYZ>> clusters;
+    for (int i = 1; i <= label; ++i) {
+        pcl::PointIndices::Ptr indices(new pcl::PointIndices);
+        for (int j = 0; j < labels.size(); ++j) {
+            if (labels[j] == i) {
+                indices->indices.push_back(j);
+                labels[j] = -1; // Remove point from labels to avoid duplicates
+            }
+        }
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud(points.makeShared());
+        extract.setIndices(indices);
+        pcl::PointCloud<pcl::PointXYZ> cluster;
+        extract.filter(cluster);
+        if (cluster.size() >= m_min_points) {
+            clusters.push_back(cluster);
+        }
+    }
+
+    // Compute the centroids of each cluster
+    pcl::PointCloud<pcl::PointXYZ> output;
+    for (const auto& cluster : clusters) {
+        pcl::PointXYZ centroid;
+        pcl::computeCentroid(cluster, centroid);
+        output.push_back(centroid);
+    }
+    return output;
 }
 
 }; // namespace pcl_detector
